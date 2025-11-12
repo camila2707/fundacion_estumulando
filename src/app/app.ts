56976ece4cc9a06fce1d8031145a7ee695e57efb
camila <camilacalfupangarcia@gmail.com>
2025-11-12 08:55:56 -0300

    correcto

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
+
+@Component({
+  selector: 'app-root',
+  standalone:true,
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('proyecto-fundacion');
+}
